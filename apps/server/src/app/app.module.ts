3fd5c643046332935f5d0c065ec6c5f74871063b
evantianx <evantianx@gmail.com>
2021-01-11 19:03:35 +0800

    feat: conncet database and add new module in workspace libs
    Signed-off-by: evantianx <evantianx@gmail.com>

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LIB_PATH } from '@space-explorer/graphql';
+import { join } from 'path';
+import { environment } from '../environments/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfig } from './database.config';
+
+const lib = join(process.cwd(), LIB_PATH);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [environment],
+    }),
+    GraphQLModule.forRoot({
+      typePaths: [join(lib, 'schemas/**/*.graphql')],
+      definitions: {
+        path: join(lib, 'lib/graphql.ts'),
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
